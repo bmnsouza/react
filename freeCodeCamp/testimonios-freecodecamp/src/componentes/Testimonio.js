20a74b97bdb344b415396ac24409ba3946f4aac9
Bruno Marcel <bmnsouza@gmail.com>
2022-10-03 00:17:37 -0300

    Proyecto 1

---
+++ freeCodeCamp/testimonios-freecodecamp/src/componentes/Testimonio.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../hojas-de-estilo/Testimonio.css';
+
+function Testimonio(props) {
+  let textoAlt = 'Foto de ' + props.nombre;
+  return (
+    <div className='contenedor-testimonio'>
+      <img 
+        className='imagen-testimonio'
+        src={require(`../imagenes/testimonio-${props.imagen}.png`)}
+        alt={textoAlt} />
+      <div className='contenedor-texto-testimonio'>
+        <p className='nombre-testimonio'>
+          <strong>{props.nombre}</strong> en {props.pais}
+        </p>
+        <p className='cargo-testimonio'>
+          {props.cargo} en <strong>{props.empresa}</strong>
+        </p>
+        <p className='texto-testimonio'>"{props.testimonio}"</p>
+      </div>
+    </div>
+  );
+}
+
+export default Testimonio;
